baa409377d6ee56a74bec3b3f3389de2b42c226c
David Lanouette <david.lanouette@gmail.com>
2014-07-30 07:14:53 -0400

    Initial adding of the Primes proj

---
+++ Primes/src/com/phoenix/com/phoenix/interview/Main.java
@@ -0,0 +1,12 @@
+package com.phoenix.com.phoenix.interview;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("In this main");
+        PrimesUi ui = new PrimesUi();
+        ui.pack();
+        ui.setVisible(true);
+
+    }
+}
